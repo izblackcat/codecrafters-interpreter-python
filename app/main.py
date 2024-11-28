52e7daa8e86faaabb9f5999dcc26cd4e547de9d7
izarkaoui <imzarkaoui@gmail.com>
2024-11-28 11:05:28 +0100

    division and comments

--- app/main.py
+++ app/main.py
@@ -15,42 +15,41 @@ def main():
     if command != "tokenize":
         print(f"Unknown command: {command}", file=sys.stderr)
         exit(1)
-        
+
     with open(filename) as file:
         file_contents = file.read()
 
     error = False
 
-
     i = 0
     while i < len(file_contents):
 
-        c = file_contents[i];
+        c = file_contents[i]
 
         line_number = file_contents.count("\n", 0, file_contents.find(c)) + 1
 
         match (c):
-            case '(':
+            case "(":
                 print("LEFT_PAREN ( null")
-            case ')':
+            case ")":
                 print("RIGHT_PAREN ) null")
-            case '{':
+            case "{":
                 print("LEFT_BRACE { null")
-            case '}':
+            case "}":
                 print("RIGHT_BRACE } null")
-            case '*':
+            case "*":
                 print("STAR * null")
-            case '.':
+            case ".":
                 print("DOT . null")
-            case ',':
+            case ",":
                 print("COMMA , null")
-            case '+':
+            case "+":
                 print("PLUS + null")
-            case '-':
+            case "-":
                 print("MINUS - null")
-            case ';':
+            case ";":
                 print("SEMICOLON ; null")
-            case '=':
+            case "=":
                 # if it's at the end then it can't be ==
                 # if it's just before the end, then we should see the last one (before it)
                 if i < len(file_contents) - 1 and file_contents[i + 1] == "=":
@@ -59,39 +58,51 @@ def main():
                     continue
                 else:
                     print("EQUAL = null")
-            case '!':
+            case "!":
                 if i < len(file_contents) - 1 and file_contents[i + 1] == "=":
                     print("BANG_EQUAL != null")
                     i += 2
                     continue
                 else:
                     print("BANG ! null")
-            case '<':
+            case "<":
                 if i < len(file_contents) - 1 and file_contents[i + 1] == "=":
                     print("LESS_EQUAL <= null")
                     i += 2
                     continue
                 else:
                     print("LESS < null")
-            
-            case '>':
+
+            case ">":
                 if i < len(file_contents) - 1 and file_contents[i + 1] == "=":
                     print("GREATER_EQUAL >= null")
                     i += 2
                     continue
                 else:
                     print("GREATER > null")
+            case "/":
+                if i < len(file_contents) - 1 and file_contents[i + 1] == "/":
+                    # IGNORE THE REST OF THE LINE : until '\n'
+                    # () // **this is a comment!**
+                    while i < len(file_contents) and file_contents[i] != "\n":
+                        i += 1
+                        continue
+                else:
+                    print("SLASH / null")
             case _:
                 error = True
-                print(f"[line {line_number}] Error: Unexpected character: {c}", file=sys.stderr)
+                print(
+                    f"[line {line_number}] Error: Unexpected character: {c}",
+                    file=sys.stderr,
+                )
         i += 1
     print("EOF  null")
-    
-    if error : 
+
+    if error:
         sys.exit(65)
     else:
         sys.exit(0)
-    
+
 
 if __name__ == "__main__":
     main()
